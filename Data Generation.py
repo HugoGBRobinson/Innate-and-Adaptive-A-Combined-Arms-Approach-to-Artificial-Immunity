a3174a1a7e44240d6dfb9b2959ed4ff114d406c1
Hugo <hugogbrobinson@gmail.com>
2022-12-29 18:56:39 +0000

    Added the ability to measure how many similar and unsimilar strings there are based on set hyper-parameters. The seed string is also randomly generated and the length can be set.

--- Data Generation.py
+++ Data Generation.py
@@ -1,11 +1,16 @@
 import random
+import string
 from random import choices
+from difflib import SequenceMatcher
 
 
 def main():
-    new_strings = generate_new_strings("Hello")
+    seed_string = ''.join(random.choices(string.ascii_uppercase + string.digits, k=50))
+    new_strings = generate_new_strings(seed_string)
+    find_similar_strings(new_strings, seed_string)
 
 def generate_new_strings(seed_string):
+
     population = [1, 2, 3, 4, 5, 6]
     weights = [0.7, 0.15, 0.1, 0.025, 0.02, 0.005]
     variations = choices(population, weights, k=100)
@@ -14,12 +19,27 @@ def generate_new_strings(seed_string):
     for variation in range(len(variations)):
         new_string = ""
         for char in seed_string:
-            if random.random() > 0.5:
+            if random.random() > 0.6:
                 new_string = new_string + chr(ord(char) + variation)
             else:
                 new_string = new_string + char
         new_strings.append(new_string)
-    print(new_strings)
+    return new_strings
+
+def find_similar_strings(new_strings, seed_string):
+    safe_strings = []
+    unsafe_strings = []
+    for string in new_strings:
+        if SequenceMatcher(None, string, seed_string).ratio() > 0.5:
+            safe_strings.append(string)
+        else:
+            unsafe_strings.append(string)
+
+    print(safe_strings)
+    print("There are " + str(len(safe_strings)) + " safe strings")
+    print(unsafe_strings)
+    print("There are " + str(len(unsafe_strings)) + " unsafe strings")
+
 
 if __name__ == '__main__':
     main()
