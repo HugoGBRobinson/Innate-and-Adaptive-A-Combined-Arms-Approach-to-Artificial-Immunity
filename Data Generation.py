6e4a7da7cf390758e0eb1fa09d738b016fa77ab0
Hugo <hugogbrobinson@gmail.com>
2023-01-09 10:00:27 +0000

    Added new, set seed strings. Improved distribution by adding 0.

--- Data Generation.py
+++ Data Generation.py
@@ -5,32 +5,42 @@ from difflib import SequenceMatcher
 
 
 def main():
-    seed_string = ''.join(random.choices(string.ascii_uppercase + string.digits, k=50))
-    new_strings = generate_new_strings(seed_string)
-    find_similar_strings(new_strings, seed_string)
+    seed_string1 = "a" * 50
+    seed_string2 = "9" * 50
+    seed_string3 = "a"
+    for i in range(50):
+        seed_string3 = seed_string3 + str(ord(seed_string3[-1]) + 1)
+    seed_string4 = ""
+    seed_string5 = ''.join(random.choices(string.ascii_uppercase + string.digits, k=50))
+
+    strings_from_string1 = generate_new_strings(seed_string1)
+    strings_from_string2 = generate_new_strings(seed_string2)
+    strings_from_string3 = generate_new_strings(seed_string3)
+    print(strings_from_string1)
+    print(strings_from_string2)
+    print(strings_from_string3)
+    # new_strings = generate_new_strings(seed_string)
+    # find_similar_strings(new_strings, seed_string)
+
 
 def generate_new_strings(seed_string):
-
-    population = [1, 2, 3, 4, 5, 6]
-    weights = [0.7, 0.15, 0.1, 0.025, 0.02, 0.005]
-    variations = choices(population, weights, k=100)
-
+    population = [0, 1, 2, 3, 4, 5, 6]
+    weights = [0.8, 0.15, 0.025, 0.02, 0.0025, 0.002, 0.005]
     new_strings = []
-    for variation in range(len(variations)):
+    for i in range(1000):
+        variations = choices(population, weights, k=len(seed_string))
         new_string = ""
-        for char in seed_string:
-            if random.random() > 0.6:
-                new_string = new_string + chr(ord(char) + variation)
-            else:
-                new_string = new_string + char
+        for i in range(len(seed_string)):
+            new_string = new_string + chr(ord(seed_string[i]) + variations[i])
         new_strings.append(new_string)
     return new_strings
+
 
 def find_similar_strings(new_strings, seed_string):
     safe_strings = []
     unsafe_strings = []
     for string in new_strings:
-        if SequenceMatcher(None, string, seed_string).ratio() > 0.5:
+        if SequenceMatcher(None, string, seed_string).ratio() > 0.1:
             safe_strings.append(string)
         else:
             unsafe_strings.append(string)
