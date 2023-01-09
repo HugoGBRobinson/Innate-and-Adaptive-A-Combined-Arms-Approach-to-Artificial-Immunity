9f7866645defb18bbd06cbe702e7d71dde875ada
Hugo <hugogbrobinson@gmail.com>
2023-01-09 10:28:14 +0000

    Variations can be positive and negative.

--- Data Generation.py
+++ Data Generation.py
@@ -16,22 +16,22 @@ def main():
     strings_from_string1 = generate_new_strings(seed_string1)
     strings_from_string2 = generate_new_strings(seed_string2)
     strings_from_string3 = generate_new_strings(seed_string3)
-    print(strings_from_string1)
-    print(strings_from_string2)
-    print(strings_from_string3)
     # new_strings = generate_new_strings(seed_string)
-    # find_similar_strings(new_strings, seed_string)
+    find_similar_strings(strings_from_string1, seed_string1)
+    find_similar_strings(strings_from_string2, seed_string2)
+    find_similar_strings(strings_from_string3, seed_string3)
 
 
 def generate_new_strings(seed_string):
     population = [0, 1, 2, 3, 4, 5, 6]
     weights = [0.8, 0.15, 0.025, 0.02, 0.0025, 0.002, 0.005]
     new_strings = []
-    for i in range(1000):
+    for i in range(100000):
         variations = choices(population, weights, k=len(seed_string))
         new_string = ""
         for i in range(len(seed_string)):
-            new_string = new_string + chr(ord(seed_string[i]) + variations[i])
+            new_string = new_string + chr(ord(seed_string[i]) + (1 * variations[i] if
+                                                                 random.random() < 0.5 else -1 * variations[i]))
         new_strings.append(new_string)
     return new_strings
 
@@ -40,7 +40,7 @@ def find_similar_strings(new_strings, seed_string):
     safe_strings = []
     unsafe_strings = []
     for string in new_strings:
-        if SequenceMatcher(None, string, seed_string).ratio() > 0.1:
+        if SequenceMatcher(None, string, seed_string).ratio() > 0.15:
             safe_strings.append(string)
         else:
             unsafe_strings.append(string)
